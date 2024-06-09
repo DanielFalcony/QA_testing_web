2ab0851c6225b5b66da73d15a50527eafc442778
Daniil <danielfalcony@gmail.com>
2024-06-09 19:16:20 +0300

    seminar_2 Done

--- hw_3/conftest.py
+++ hw_3/conftest.py
@@ -2,8 +2,8 @@ import pytest
 import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdrivermanager_cn.chrome import ChromeDriverManager
-from webdrivermanager_cn.geckodriver import GeckodriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 with open("./testdata.yaml") as f:
@@ -14,7 +14,7 @@ with open("./testdata.yaml") as f:
 @pytest.fixture(scope="session")
 def browser():
     if browser == "firefox":
-        service = Service(executable_path=GeckodriverManager().install())
+        service = Service(executable_path=GeckoDriverManager().install())
         options = webdriver.FirefoxOptions()
         driver = webdriver.Firefox(service=service, options=options)
     else:
